a6fda11cc265240557e7f61100416867ae72f109
Azzeri <azmario2698@gmail.com>
2023-01-13 19:54:50 +0100

    eq services refresh and display bug workaround

--- resources/js/Components/EqItem/Services.vue
+++ resources/js/Components/EqItem/Services.vue
@@ -16,7 +16,12 @@ itemData.services.forEach((service) => {
 const finishService = (index) => {
     const form = serviceForms[index];
 
-    form.put(route("eqServices.finish", itemData.services[index].id));
+    form.put(route("eqServices.finish", itemData.services[index].id), {
+        preserveScroll: true,
+        onSuccess: () => {
+            form.reset(), form.clearErrors(), location.reload();
+        },
+    });
 };
 
 const isDateAfterDue = (date) => {
@@ -28,43 +33,45 @@ const isDateAfterDue = (date) => {
 </script>
 <template>
     <template class="grid grid-cols-4 gap-2">
-        <form
-            v-for="(service, index) in itemData.services"
-            @submit.prevent="finishService(index)"
-        >
-            <div class="card w-86 bg-base-100 shadow">
-                <div class="card-body">
-                    <h2 class="card-title">
-                        {{ service.eq_service_template.name }}
-                    </h2>
-                    <h3
-                        class="font-semibold"
-                        :class="{
-                            'text-error': isDateAfterDue(
-                                service.expected_perform_date
-                            ),
-                        }"
-                    >
-                        {{
-                            `${__("expected")}: ${
-                                service.expected_perform_date
-                            }`
-                        }}
-                    </h3>
-                    <p class="text-justify truncate">
-                        {{ service.eq_service_template.description }}
-                    </p>
-                    <div class="card-actions justify-end">
-                        <button
-                            :disabled="serviceForms[index].processing"
-                            type="submit"
-                            class="btn btn-primary btn-sm"
+        <template v-for="(service, index) in itemData.services">
+            <form
+                v-if="!service.actual_perform_date"
+                @submit.prevent="finishService(index)"
+            >
+                <div class="card w-86 bg-base-100 shadow">
+                    <div class="card-body">
+                        <h2 class="card-title">
+                            {{ service.eq_service_template.name }}
+                        </h2>
+                        <h3
+                            class="font-semibold"
+                            :class="{
+                                'text-error': isDateAfterDue(
+                                    service.expected_perform_date
+                                ),
+                            }"
                         >
-                            {{ __("finish") }}
-                        </button>
+                            {{
+                                `${__("expected")}: ${
+                                    service.expected_perform_date
+                                }`
+                            }}
+                        </h3>
+                        <p class="text-justify truncate">
+                            {{ service.eq_service_template.description }}
+                        </p>
+                        <div class="card-actions justify-end">
+                            <button
+                                :disabled="serviceForms[index].processing"
+                                type="submit"
+                                class="btn btn-primary btn-sm"
+                            >
+                                {{ __("finish") }}
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </form>
+            </form>
+        </template>
     </template>
 </template>
